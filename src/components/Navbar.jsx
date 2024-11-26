ac4b7ebb51636357eab57935ab23beb430d9fbd4
Tushar Negi <negitushar81@gmail.com>
2024-11-26 11:58:45 +0530

    navbar for pc and mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,43 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { styles } from '../styles'
 import { navLinks } from '../constants'
-import { logo, menu, close } from '../assets'
+import { menu, close } from '../assets'
+import logo from '../../public/logo.png'
 
 const Navbar = () => {
+
+  const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
+
   return (
-    <nav></nav>
+    <nav className={` ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}>
+          <img src={logo} alt="logo" className='w-9 h-9 object-cover rounded-full ' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Tushar Negi</p>
+        </Link>
+
+        {/* for pc view */}
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li key={link.id} className={` ${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer `}
+              onClick={() => setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+
+        {/* for mobile view */}
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle ? close : menu} alt="menu" className='w-[18px] h-[18px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
+        </div>
+      </div>
+    </nav>
   )
 }
 
